d9a5873170ca6d29dd049e8166b25c25e685529f
supriyobaruab <suxpriyo@gmail.com>
2025-01-16 10:24:35 +0600

    Added the question

--- 2nd.cpp
+++ 2nd.cpp
@@ -1,3 +1,4 @@
+/*Create a C++ program to manage staff salaries for a farm. Define a struct named Staff, which includes the attributes- stfID and monthlyWages. Implement methods within the struct-setData to set the staff information, displayData to display the staff details. Additionally, include an operational method called yearlyWagesWithBonus() to calculate the yearly wages with bonuses for each staff. The company provides two bonuses per year, each equal to 50 percent of the monthly wages. In the main function, demonstrate the usage of the Staff struct by creating an instance named s1, setting details, displaying the product information, and showing the yearly wages of this staff with 2 bonuses.*/
 #include <iostream>
 using namespace std;
 struct Staff
